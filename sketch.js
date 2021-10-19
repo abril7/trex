9418183a93ee30f5e5270f8efa594ebbcd1e482c
abril7 <92388606+abril7@users.noreply.github.com>
2021-10-19 16:42:29 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+var trex, trex_running, trex_collided;
+var ground, invisibleGround, groundImage;
+
+var cloud, cloudsGroup, cloudImage;
+
+
+function preload(){
+
+ 
+}
+
+function setup() {
+  createCanvas(600, 200);
+}
+
+function draw() {
+  background(180);
+  
+}
+
